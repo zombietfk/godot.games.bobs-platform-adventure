9af7016cd40d84d6152af97268dfc12a3db04e62
Red Dead Lion <sam.m.w.judge+red.dead.lion@googlemail.com>
2025-11-22 19:13:45 -0600

    Fixed machine gun timer

--- objects/mounted_machine_gun/mounted_machine_gun.gd
+++ objects/mounted_machine_gun/mounted_machine_gun.gd
@@ -6,12 +6,16 @@ extends Node2D;
 @export var machine_gun_bullet_direction: Vector2 = Vector2.RIGHT;
 @export_flags_2d_physics var machine_gun_bullet_collisison_mask;
 
-func _process(_delta: float) -> void:
-	if is_firing:
-		await get_tree().create_timer(3).timeout;
+var shoot_timer = 0.3;
+var c_shoot_timer = 0;
+
+func _process(delta: float) -> void:
+	c_shoot_timer += delta;
+	if is_firing and c_shoot_timer > shoot_timer:
+		c_shoot_timer = 0;
 		var m_gun_bullet: MountedMachineGunBullet = machine_gun_bullet.instantiate();
 		Main.instance.add_child(m_gun_bullet);
-		m_gun_bullet.global_position = global_position;
+		m_gun_bullet.global_position = $Marker2D.global_position;
 		m_gun_bullet.movement_direction = machine_gun_bullet_direction;
 		m_gun_bullet.collision_raycast.collision_mask = machine_gun_bullet_collisison_mask;
 		print(m_gun_bullet.collision_raycast);
