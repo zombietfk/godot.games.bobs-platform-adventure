9875a31f7e5c2d04b6b30a5cf5bf86d87d9a8181
Red Dead Lion <sam.m.w.judge+red.dead.lion@googlemail.com>
2026-01-21 21:18:01 -0600

    Added level 37 to game

--- objects/switch/switch_area.gd
+++ objects/switch/switch_area.gd
@@ -1,6 +1,14 @@
 class_name SwitchArea;
 extends AbstractSwitchArea;
 
+@onready var collision_shape = $CollisionShape2D;
+
+func _ready()->void:
+	if !body_entered.is_connected(_on_body_entered):
+		body_entered.connect(_on_body_entered);
+	if !body_exited.is_connected(_on_body_exited):
+		body_entered.connect(_on_body_entered);
+
 func _on_switch_on():
 	pass;
 
